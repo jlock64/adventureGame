984970078dd14cdfdb7dfadf5dec621e604a3249
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-02-11 15:44:49 -0500

    cleaned up my code by adding objects and extra functions

--- adventure.js
+++ adventure.js
@@ -1,250 +1,271 @@
-var confirmPlay = confirm("Get ready for the adventure of a lifetime!")
-  // if (confirmPlay === "") {
-  //   console.log("You must select OK or hit cancel.")
-  // }
 
-var refreshPage = function () {
-  confirm("Please refresh this page to take a new adventure path!");
-}
 
-  /////////////////////////SAFARI GAME CHOICE//////////////////////
-var answer = prompt("Are you ready to go on a safari? Type yes or no. If you'd rather play some trivia, type trivia.");
-  if (answer === "yes") {
-    confirm("Get ready for a rocking good time! We are headed deep into the untamed world of Namibia! Buckle up and please keep your hands inside the vehicle at all times. We are not responsible for any forcefully removed body parts.");
-    answer = prompt("What animals are you hoping to see most? Lions,  elephants or giraffes?");
+    // START OF GAME //
+    var startGame = function() {
+    alert("Get ready for the adventure of a lifetime!")
 
-     //ANSWER EQUALS LIONS//
-     if (answer === "lions" || answer === "Lions") {
-       answer = prompt("Awesome! Lions are cool. Do you think you could  beat a lion in a foot race?");
-       if (answer === "yes") {
-         confirm("You are sadly mistaken my friend. Lions can reach speeds of 36mph! Even the world's fastest man Usain Bolt, who has been clocked at 28mph couldn't escape a lions attack!");
-         refreshPage();
-       }
-       else if (answer === "no") {
-         confirm("You are right-o lion master tamer! Lions can reach speeds of 36mph!");
-         refreshPage();
-       }
-       else {
-         confirm("Sorry that was not a correct input.")
-         refreshPage();
-       }
+    //  SAFARI GAME CHOICE //
+    var answer = prompt(
+      "Are you ready to go on a safari? Type yes or no. If you'd rather play some trivia, type trivia.").trim().toLowerCase();
+    if (answer === "yes") {
+      alert(
+        "Get ready for a rocking good time! We are headed deep into the untamed world of Namibia! Buckle up and please keep your hands inside the vehicle at all times. We are not responsible for any forcefully removed body parts.");
 
-     //ANSWER EQUALS LIONS//
-  }  else if (answer === "elephants") {
-       confirm("Cool! Me too! Elephants are the largest land animals in the world!");
-       answer = prompt("Let me test your elephant knowledge. How much can an average elephant heart weigh? 27 pounds or 46 pounds?");
+      function animalChoice() {
+      answer = prompt("What animals are you hoping to see most? Lions, elephants or giraffes?").trim().toLowerCase();
+
+      // LION ANSWER //
+      if (answer === "lions" || answer ==="lion") {
+        function lionQuestion () {
+        answer = prompt("Awesome! Lions are cool. Do you think you could beat a lion in a foot race?").trim().toLowerCase();
+        if (answer === "yes") {
+          alert(lionAnswer.yes);
+          alert("Lets play again!");
+          startGame();
+        } else if (answer === "no") {
+          alert(lionAnswer.no);
+          alert("Lets play again!");
+          startGame();
+        } else {
+          alert(lionAnswer.unknown);
+          lionQuestion();
+        }}
+        lionQuestion();
+
+        // ELEPHANT ANSWER //
+      } else if (answer === "elephants" || answer ==="elephant") {
+        alert("Cool! Me too! Elephants are the largest land animals in the world!");
+        function elephantQuestion () {
+        answer = prompt("Let me test your elephant knowledge. How much can an average elephant heart weigh? 27 pounds or 46 pounds?").trim().toLowerCase();
         if (answer === "27" || answer === "27 pounds") {
-          confirm("Nope. An average elephant heart can weigh up to 46 pounds! That's a big heart throb!");
-          refreshPage();
+          alert(elephantAnswer.no);
+          alert("Lets play again!");
+          startGame();
+        } else if (answer === "46" || answer === "46 pounds") {
+          alert(elephantAnswer.yes);
+          alert("Lets play again!");
+          startGame();
+        } else {
+          alert(elephantAnswer.unknown);
+          elephantQuestion();
         }
-        else if (answer === "46" || answer === "46 pounds") {
-          confirm("That is right oh wise one! The average elephant heart can weigh up to 46 pounds! That's a big pumper!");
-          refreshPage();
-        }
-        else {
-          confirm("Sorry, that was not a choice. You lose.");
-          refreshPage();
-        }
-  }
+      }
+      elephantQuestion();
 
-     //ANSWER EQUALS GIRAFFES//
-     else if (answer === "giraffes") {
-       confirm("Sweet! Giraffes are the tallest mammal in the world!");
-       answer = prompt("Let me test your giraffe knowledge. On average do giraffes get more or less sleep than the average humans 6.5 hours of sleep?");
+      // GIRAFFE ANSWER //
+      } else if (answer === "giraffes" || answer === "giraffe") {
+        alert("Sweet! Giraffes are the tallest mammal in the world!");
+        function giraffeQuestion() {
+        answer = prompt("Let me test your giraffe knowledge. On average do giraffes get more or less sleep than the average humans 6.5 hours of sleep?").trim().toLowerCase();
         if (answer === "more") {
-          confirm("Wrong! Giraffes get on average less than 2 hours per day of sleep! They must drink lots of coffee.");
-          refreshPage();
-        }
-        else if (answer === "less") {
-          confirm("Whoa weirdo! Had no clue you knew so much about giraffes. Actually that's pretty cool. Knowledge is power bro.");
-          refreshPage();
-        }
-        else {
-          confirm("Sorry that was not a choice. But giraffes get on average less than 2 hours per day of sleep! They must drink lots of coffee.");
-          refreshPage();
-        }
-     }
+          alert(giraffeAnswer.more);
+          alert("Lets play again!");
+          startGame();
+        } else if (answer === "less") {
+          alert(giraffeAnswer.less);
+          alert("Lets play again!");
+          startGame();
+        } else {
+          alert(giraffeAnswer.unknown);
+          giraffeQuestion();
+        }}
+        giraffeQuestion();
 
-    // WRONG INPUT //
-     else {
-       confirm("That was not a choice. But I hope you get to see what you want on the safari!");
-       refreshPage();
-     }
-} // END OF SAFARI GAME //
+      // WRONG INPUT //
+      } else {
+        alert("That was not a choice. Pick again.");
+        animalChoice();
+      }
+    }
+      animalChoice();
+    }
+     // END OF SAFARI GAME //
 
-  /////////////////////START DRINKING GAME///////////////////////
-  else if (answer === "no") {
-      answer = prompt("You are a party pooper! Go back where you came from! I'm sorry I didn't mean that. Would you rather go get drunk?");
+    // START DRINKING GAME //
+    else if (answer === "no") {
+      answer = prompt("You are a party pooper! I'm sorry I didn't mean that. Would you rather go get drunk?").trim().toLowerCase();
 
       // YES TO DRINKING //
       if (answer === "yes") {
-        answer = prompt("I like drinking too. We are going to get along just fine. Do you prefer beer or wine?");
+        function beerWineQuestion() {
+        answer = prompt("I like drinking too. We are going to get along just fine. Do you prefer beer or wine?").trim().toLowerCase();
 
         // BEER CHOICE //
         if (answer === "beer") {
-          confirm("I love beer too! Be careful not to drink too much or you will get a beer belly. Hairy beer bellies aren't pretty.");
-          answer = prompt("Do you prefer IPA's or stouts?");
-          if (answer === "ipas" || answer === "IPAs" || answer === "IPAS") || answer === "ipa's" {
-            confirm("That's what I'm talking about! Unfortunately IPAs average more than 200 calories per beer. Guess we will starworking on our big hairy bellies together.");
-            refreshPage();
-          }
-          else if (answer === "stouts") {
-            confirm("I used to be a big fan of stouts. Now I just think they are thick and milky. That sounds gross.");
-            refreshPage();
-          }
-          else {
-            confirm("That wasn't a choice but any beer I will try at least once.");
-            refreshPage();
+          alert("I love beer too! Be careful not to drink too much or you will get a beer belly. Hairy beer bellies aren't pretty.");
+          function beerQuestion() {
+          answer = prompt("Do you prefer IPA's or stouts?").trim().toLowerCase();
+          if (answer === "ipas" || answer === "ipa's") {
+            alert(beerAnswer.ipas);
+            alert("Lets play again!");
+            startGame();
+          } else if (answer === "stouts") {
+            alert(beerAnswer.stouts);
+            alert("Lets play again!");
+            startGame();
+          } else {
+            alert(beerAnswer.unknown);
+            beerQuestion();
           }
         }
+        beerQuestion();
+      }
 
         // WINE CHOICE //
-        else if (answer === "wine") {
-          confirm("Ok I like wine too. I usually only drink it with my wife.");
-          answer = prompt("Do you prefer white or red?");
+          else if (answer === "wine") {
+          alert("Ok I like wine too. I usually only drink it with my wife.");
+          function winePreference () {
+            answer = prompt("Do you prefer white or red?").trim().toLowerCase();
             if (answer === "white") {
-              confirm("I am not a big fan of white wine. But I do occasionally drink it in the summer.");
-              refreshPage();
-            }
-            else if (answer === "red") {
-              confirm("Awh yeah! If it's red I will drink it!");
-              refreshPage();
-            }
-            else {
-              confirm("Not sure about that answer.");
-              refreshPage();
-            }
-         }
+              alert(wineChoice.white);
+              alert("Lets play again!");
+              startGame();
+            } else if (answer === "red") {
+              alert(wineChoice.red);
+              alert("Lets play again!");
+              startGame();
+            } else {
 
-         else {
-           confirm("That was not a choice but hey, I like to indulge in most alcohol.");
-           refreshPage();
-         }
+              alert(wineChoice.other);
+              winePreference();
+              }
+          }
+          winePreference();
+
+          } else {
+          alert("That was not a choice but hey, I like to indulge in most alcohol. Try again.");
+          beerWineQuestion();
+        }
+       }
+       beerWineQuestion();
       }
-
       // NO TO DRINKING //
       else if (answer === "no") {
-        confirm("Ok so you don't like to drink. That's totally cool. I'm sure we can find something in common.");
-        answer = prompt("I love surfing and golfing. If you had to pick one which one would you want to do with me?");
-          if (answer === "surfing") {
-            confirm("Tubular dude! Let's go!");
-            answer = prompt("Are you afraid of sharks?");
-              if (answer === "yes") {
-                confirm("Chances are that you won't die. The odds of getting attacked and killed by a shark are 1 in nearly 4 million!");
-                refreshPage();
-              }
-              else if (answer === "no") {
-                confirm("You shouldn't be scared. The odds of getting attacked and killed by a shark are 1 in nearly 4 million!");
-                refreshPage();
-              }
-              else {
-                confirm("I don't recognize that answer.");
-                refreshPage();
-              }
+        alert("Ok so you don't like to drink. That's totally cool. I'm sure we can find something in common.");
+        function surfGolfQuestion() {
+        answer = prompt("I love surfing and golfing. If you had to pick one which one would you want to do with me?").trim().toLowerCase();
+
+        if (answer === "surfing" || answer === "surf") {
+          alert("Tubular dude! Let's go!");
+         function sharkQuestion() {
+          answer = prompt("Are you afraid of sharks?").trim().toLowerCase();
+          if (answer === "yes") {
+            alert(sharkAnswer.yes);
+            alert("Lets play again!");
+            startGame();
+          } else if (answer === "no") {
+            alert(sharkAnswer.no);
+            alert("Lets play again!");
+            startGame();
+          } else {
+            alert(sharkAnswer.other);
+            sharkQuestion();
           }
-          else if (answer === "golfing") {
-            confirm("Sweet, I love to golf! Unfortunately my driving sucks. We might be searching for lots of balls. That sounds weird.");
-            answer = prompt("Lets talk about your handicap in golf. Mine is 90. Is yours above or below that?");
-              if (answer === "above") {
-                confirm("Yikes! We might be searching for your balls more than mine.");
-                refreshPage();
-              }
-              else if (answer === "below") {
-                confirm("Cool, you are pretty good. Let's make some bets!");
-                refreshPage();
-              }
-              else {
-                confirm("You must answer with below or above.");
-                refreshPage();
-              }
-          }
-          else {
-            answer = prompt("I guess we are going to have to find something else to do.");
-            refreshPage();
-          }
+         }
+         sharkQuestion();
+
+       } else if (answer === "golfing" || answer === "golf") {
+          alert("Sweet, I love to golf! Unfortunately my driving sucks. We might be searching for lots of balls. That sounds weird.");
+          function golfQuestion () {
+          answer = prompt("Lets talk about your handicap in golf. Mine is 90. Is yours above or below that?").trim().toLowerCase();
+            if (answer === "above") {
+            alert(golfAnswer.above);
+            alert("Lets play again!");
+            startGame();
+          } else if (answer === "below") {
+            alert(golfAnswer.below);
+            alert("Lets play again!");
+            startGame();
+          } else {
+            alert(golfAnswer.other);
+            golfQuestion();
+          }}
+          golfQuestion();
+
+        } else {
+          alert("Awh come on be adventerous. Pick one please.")
+          surfGolfQuestion();
+        }
        }
-
-       // UNKNOWN INPUT //
-  else {
-         confirm("Sorry, that was a yes or no question.");
-         refreshPage();
-  }
-} // END OF DRINKING GAME //
-
-////////////////// UNKNOWN INPUT ON SAFARI QUESTION ////////////////
-
-    else {
-
-      var antarticaTrivia = function() {
-        answer = prompt("Which continent is the only one without reptiles or snakes?");
-        }
-
-      var batTrivia = function() {
-        answer = prompt("Which direction do bats always turn out of a cave? Left or right?");
-        }
-
-      var lotteryTrivia = function() {
-        answer = prompt("What do 100% of all lottery winners do?");
-        }
-
-      var marriedMenTrivia = function() {
-        answer = prompt("Married men revealed that they do this twice as often as single men.");
-        }
-
-      confirm("Ok smartypants! Let's play trivia! Hit OK to continue.");
-      antarticaTrivia();
-
-      if (answer === "antartica" || answer === "Antartica") {
-        confirm("Correct-o smarty pants!");
+       surfGolfQuestion();
       }
+      // UNKNOWN INPUT //
       else {
-        confirm("Wrong answer. I will give you one more try.");
+        alert("Sorry, that was a yes or no question.");
+        alert("Lets play again!");
+
+        startGame();
+      }
+    } // END OF DRINKING GAME //
+
+    // TRIVIA FUNCTIONS //
+    else if (answer === "trivia") {
+      var antarticaTrivia = function() {
+        answer = prompt("Which continent is the only one without reptiles or snakes?").trim().toLowerCase();
+      }
+      var batTrivia = function() {
+        answer = prompt("Which direction do bats always turn out of a cave? Left or right?").trim().toLowerCase();
+      }
+      var lotteryTrivia = function() {
+        answer = prompt("What do 100% of all lottery winners do?").trim().toLowerCase();
+      }
+      var marriedMenTrivia = function() {
+        answer = prompt(
+          "Married men revealed that they do this twice as often as single men.").trim().toLowerCase();
+      }
+
+      // BEGIN TRIVIA //
+      alert("Ok smartypants! Let's play trivia! Hit OK to continue.");
+      antarticaTrivia();
+      if (answer === "antartica" || answer === "Antartica") {
+        alert(antarticaAnswer.right);
+      } else {
+        alert(antarticaAnswer.wrong);
         antarticaTrivia();
         if (answer === "antartica") {
-          confirm("Correct-o smarty pants!");
-        }
-        else {
-          confirm("Wrong again. The correct answer is Antartica.")
+          alert(antarticaAnswer.right);
+        } else {
+          alert(antarticaAnswer.wrongPlusAns)
         }
       }
 
       batTrivia();
       if (answer === "left") {
-        confirm("Right! I mean left is the right answer. Good work.")
-      }
-      else {
-        confirm("Nope. They always turn left. Weirdos!")
+        alert("Right! I mean left is the right answer. Good work.")
+      } else {
+        alert("Nope. They always turn left. Weirdos!")
       }
 
       lotteryTrivia();
-      if (answer === "gain weight" || answer === "get fat" || answer === "get fatter") {
-        confirm("That's right big boy! Doesn't that make you feel just a bit better about not winning the lottery?")
-      }
-      else {
-        confirm("Sorry wrong answer. Try one more time.")
+      if (answer === "gain weight" || answer === "get fat" || answer ==="get fatter") {
+        alert(lotteryAnswer.right);
+      } else {
+        alert(lotteryAnswer.wrong)
         lotteryTrivia();
-        if (answer === "gain weight" || answer === "get fat" || answer === "get fatter") {
-          confirm("That's right big boy! Doesn't that make you feel just a bit better about not winning the lottery?")
-        }
-        else {
-          confirm("Sorry, wrong again. 100% of all lottery winners gain weight. Doesn't that make you feel just a bit better about not winning the lottery?");
+        if (answer === "gain weight" || answer === "get fat" || answer ==="get fatter") {
+          alert(lotteryAnswer.right);
+        } else {
+          alert(lotteryAnswer.wrongAgain);
         }
       }
 
       marriedMenTrivia();
       if (answer === "change their underwear" || answer === "change underwear") {
-        confirm("That's right! Single men have smellier underwear!");
-      }
-      else {
-        confirm("Wrong answer! Try again. Think brainiac.");
+        alert(marMenAnswer.right);
+      } else {
+        alert(marMenAnswer.wrong);
         marriedMenTrivia();
         if (answer === "change their underwear" || answer === "change underwear") {
-          confirm("That's right! Single men have smellier underwear!");
+          alert(marMenAnswer.right);
+        } else {
+          alert(marMenAnswer.wrongAgain);
         }
-        else {
-          confirm("Wrong answer! Married men change their underwear twice as often as single men.");
       }
+      alert("Lets play again!");
+      startGame();
+    } else {
+      alert("That's not a choice buddy! Come on and be adventerous!")
+      startGame();
     }
-      refreshPage();
-
-    }
+  } // End of startGame function //
+startGame();
---
+++ objects.js
@@ -0,0 +1,65 @@
+// OBJECTS //
+
+// ANIMAL ANSWERS //
+  var lionAnswer = {
+    yes: "You are sadly mistaken my friend. Lions can reach speeds of 36mph! Even the world's fastest man Usain Bolt, who has been clocked at 28mph couldn't escape a lions attack!",
+    no: "You are right-o lion master tamer! Lions can reach speeds of 36mph!",
+    unknown: "Sorry that was not a choice. Try again."
+  }
+  var elephantAnswer = {
+    yes: "That is right oh wise one! The average elephant heart can weigh up to 46 pounds! That's a big pumper!",
+    no: "Nope. An average elephant heart can weigh up to 46 pounds! That's a big heart throb!",
+    unknown: "That was not a choice. Try again."
+  }
+  var giraffeAnswer = {
+      more: "Wrong! Giraffes get on average less than 2 hours per day of sleep! They must drink lots of coffee.",
+      less: "Whoa weirdo! Had no clue you knew so much about giraffes. Actually that's pretty cool. Knowledge is power bro.",
+      unknown: "You have to answer either less or more. Try again my friend."
+    }
+
+  // BEER ANSWERS //
+  var beerAnswer = {
+    ipas: "That's what I'm talking about! Unfortunately IPAs average more than 200 calories per beer. Guess we will starworking on our big hairy bellies together.",
+    stouts: "I used to be a big fan of stouts. Now I just think they are thick and milky. That sounds gross.",
+    unknown: "That wasn't a choice but any beer I will try at least once. Choose again."
+  }
+
+  // WINE CHOICES //
+  var wineChoice = {
+    red: "Awh yeah! If it's red I will drink it!",
+    white: "I am not a big fan of white wine. But I do occasionally drink it in the summer.",
+    other: "Not sure about that answer. Enter either red or white."
+  }
+
+  // SHARK ANSWERS //
+  var sharkAnswer = {
+    yes: "Chances are that you won't die. The odds of getting attacked and killed by a shark are 1 in nearly 4 million!",
+    no: "You shouldn't be scared. The odds of getting attacked and killed by a shark are 1 in nearly 4 million!",
+    other: "Please answer yes or no."
+  }
+
+  // GOLF ANSWERS //
+  var golfAnswer = {
+    above: "Yikes! We might be searching for your balls more than mine.",
+    below: "Cool, you are pretty good. Let's make some bets!",
+    other: "You must answer with below or above."
+  }
+
+  // TRIVIA ANSWERS //
+  var antarticaAnswer = {
+    right: "Correct-o smarty pants!",
+    wrong: "Wrong answer. I will give you one more try.",
+    wrongPlusAns: "Wrong again. The correct answer is Antartica."
+  }
+
+  var lotteryAnswer = {
+    right: "That's right big boy! Doesn't that make you feel just a bit better about not winning the lottery?",
+    wrong: "Sorry wrong answer. Try one more time.",
+    wrongAgain: "Sorry, wrong again. 100% of all lottery winners gain weight. Doesn't that make you feel just a bit better about not winning the lottery?"
+  }
+
+  var marMenAnswer = {
+    right: "That's right! Single men have smellier underwear!",
+    wrong: "Wrong answer! Try again. Think brainiac.",
+    wrongAgain: "Wrong answer! Married men change their underwear twice as often as single men."
+  }  
